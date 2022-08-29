ece0dd6a2c5dce04b65fd1ba783a5d9be74ce21a
Desor Guishny <desorguishny@gmail.com>
2022-08-29 18:19:48 -0400

    Fix linter errors

--- person.rb
+++ person.rb
@@ -1,10 +1,10 @@
 # implementation person class
 class Person
-  def initialize(age, _parent_permission: true, name: 'Unknown')
+  def initialize(age, parent_permission: true, name: 'Unknown')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
-    @parent_permission = _parent_permission
+    @parent_permission = parent_permission
   end
 
   # Getter for id
@@ -14,6 +14,7 @@ class Person
   attr_accessor :age
 
   private
+
   def of_age?
     @age >= 18
   end
